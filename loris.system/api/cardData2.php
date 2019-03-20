38a7d6a56704dbda6a2ee368c988e90bb5710ba2
LeonardoSanBenitez <lsBenitezPereira@gmail.com>
2019-03-20 16:08:50 -0300

    table done

---
+++ loris.system/api/cardData2.php
@@ -0,0 +1,12 @@
+{
+    "title": "Cumprimento à meta",
+    "yTitle": "Produtividade",
+    "yMin": 0,
+    "yMax": 200,
+    "colors": [
+        [0.1, "#DF5353"],
+        [0.5, "#DDDF0D"],
+        [0.9, "#55BF3B"]
+    ],
+    "data": 81.5
+}
